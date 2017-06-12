d5db49b34f139b4548ba1bd1a37ecf6fdde8b1a7
Laurent Rochette <lrochette@gmail.com>
2017-06-12 10:59:16 -0500

    - adding reset script for SQL commands

---
+++ reset.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE ecloudintegration_ref
+GO
+
+USE ecloudintegration_ref
+CREATE SCHEMA tracking AUTHORIZATION dbo
+GO
+
+CREATE DATABASE ecloudintegration_dev
+GO
+
+USE ecloudintegration_dev
+CREATE SCHEMA tracking AUTHORIZATION dbo
+GO
--- resetDatabases.bat
+++ resetDatabases.bat
@@ -10,10 +10,5 @@ set /p PASSWD="Enter PASSWD: "
 sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_ref" -U lrochette -P %PASSWD%
 sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_dev" -U lrochette -P %PASSWD%
 
-ECHO -----RECREATE DATABASES-----
-sqlcmd -S localhost -Q "CREATE DATABASE ecloudintegration_ref" -U lrochette -P %PASSWD%
-sqlcmd -S localhost -Q "CREATE DATABASE ecloudintegration_dev" -U lrochette -P %PASSWD%
-
-ECHO -----RECREATE SCHEMA-----
-sqlcmd -S localhost -Q "USE ecloudintegration_ref ; CREATE SCHEMA tracking AUTHORIZATION dbo" -U lrochette -P %PASSWD%
-sqlcmd -S localhost -Q "USE ecloudintegration_dev ; CREATE SCHEMA tracking AUTHORIZATION dbo" -U lrochette -P %PASSWD%
+ECHO -----RECREATE DATABASES and SCHEMAS-----
+sqlcmd -S localhost -i reset.sql   -U lrochette -P %PASSWD%
