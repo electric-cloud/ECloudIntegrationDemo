de192787cbce078d4f17da43c68257f8cd8723af
Laurent Rochette <lrochette@gmail.com>
2017-06-12 10:54:04 -0500

    - Add schema creation

--- resetDatabases.bat
+++ resetDatabases.bat
@@ -13,3 +13,7 @@ sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_dev" -U lrochette -P %PA
 ECHO -----RECREATE DATABASES-----
 sqlcmd -S localhost -Q "CREATE DATABASE ecloudintegration_ref" -U lrochette -P %PASSWD%
 sqlcmd -S localhost -Q "CREATE DATABASE ecloudintegration_dev" -U lrochette -P %PASSWD%
+
+ECHO -----RECREATE SCHEMA-----
+sqlcmd -S localhost -Q "USE ecloudintegration_ref ; CREATE SCHEMA tracking AUTHORIZATION dbo" -U lrochette -P %PASSWD%
+sqlcmd -S localhost -Q "USE ecloudintegration_dev ; CREATE SCHEMA tracking AUTHORIZATION dbo" -U lrochette -P %PASSWD%
