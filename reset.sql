793a58f47cb1891cc99f4c860aad3925bf6a3600
Laurent Rochette <lrochette@datical.com>
2017-06-12 13:37:46 -0500

    - assign roles

--- reset.sql
+++ reset.sql
@@ -1,14 +1,64 @@
+-- create login
+USE [master]
+GO
+CREATE LOGIN [datical_login] WITH PASSWORD=N'datical_login', DEFAULT_DATABASE=[ecloudintegration_ref], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+GO
+GRANT VIEW ANY DEFINITION TO datical_login
+GO
 
+-- create REF database and schema
 CREATE DATABASE ecloudintegration_ref
 GO
 
-USE ecloudintegration_ref
+USE ecloudintegration_ref;
+GO
 CREATE SCHEMA tracking AUTHORIZATION dbo
 GO
 
+
+CREATE USER [datical_user] FOR LOGIN [datical_login]
+GO
+EXEC sp_addrolemember N'db_ddladmin', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_datareader', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_datawriter', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_securityadmin', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_backupoperator', N'datical_user'
+GO
+GRANT VIEW DATABASE STATE to [datical_user]
+GO
+ALTER SERVER ROLE [dbcreator] ADD MEMBER [datical_user]
+GO
+EXEC sp_addrolemember N'db_owner', N'datical_user'
+GO
+
+-- create DEV database and schema
 CREATE DATABASE ecloudintegration_dev
 GO
 
-USE ecloudintegration_dev
+USE ecloudintegration_dev;
+GO
 CREATE SCHEMA tracking AUTHORIZATION dbo
+GO
+
+CREATE USER [datical_user] FOR LOGIN [datical_login]
+GO
+EXEC sp_addrolemember N'db_ddladmin', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_datareader', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_datawriter', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_securityadmin', N'datical_user'
+GO
+EXEC sp_addrolemember N'db_backupoperator', N'datical_user'
+GO
+GRANT VIEW DATABASE STATE to [datical_user]
+GO
+ALTER SERVER ROLE [dbcreator] ADD MEMBER [datical_user]
+GO
+EXEC sp_addrolemember N'db_owner', N'datical_user'
 GO
--- resetDatabases.bat
+++ resetDatabases.bat
@@ -5,10 +5,11 @@ REM DELETE databaseBackupRestoreLocation
 REM
 ECHO -----DROP DATABASES-----
 
-set /p PASSWD="Enter PASSWD: "
+set user="system"
+set /p PASSWD="Enter PASSWD for %user%: "
 
-sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_ref" -U lrochette -P %PASSWD%
-sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_dev" -U lrochette -P %PASSWD%
+sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_ref" -U %user% -P %PASSWD%
+sqlcmd -S localhost -Q "DROP DATABASE ecloudintegration_dev" -U %user% -P %PASSWD%
 
 ECHO -----RECREATE DATABASES and SCHEMAS-----
-sqlcmd -S localhost -i reset.sql   -U lrochette -P %PASSWD%
+sqlcmd -S localhost -i reset.sql   -U %user% -P %PASSWD%
