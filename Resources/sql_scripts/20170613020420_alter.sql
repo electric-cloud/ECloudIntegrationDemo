65dbee77d862e416c79fe1de0a42040adb88fbb4
pluginsdev@electric-cloud.com <pluginsdev@electric-cloud.com>
2017-06-13 02:04:29 +0000

    Datical DB Deployment Packager Deployment Pipeline: ECloudPipeline Deploy / Rollback Scripts Processed (Associated Labels): ddl\alter.sql / none (utf8,alter.sql,$all)

---
+++ Resources/sql_scripts/20170613020420_alter.sql
@@ -0,0 +1,13 @@
+USE [ecloudintegration_ref];
+
+:OUT "C:\datical\ECloudIntegrationDemo\Resources\sql_scripts\20170613020420_alter.log"
+/* setlocale (LC_ALL, 'nl_BE');*/
+
+
+/*
+SET IDENTITY_INSERT [dbo].[francais] ON
+INSERT [dbo].[francais] ([ID], [nom], [prénom]) VALUES (1, 'Rochette', 'laurent')
+INSERT [dbo].[francais] ([ID], [nom], [prénom]) VALUES (2, 'Chabot', 'Geneviève')
+*/
+
+:OUT stdout
