965a085203432bffbb1bea1cca5c01006eea877e
pluginsdev@electric-cloud.com <pluginsdev@electric-cloud.com>
2017-06-15 16:16:03 +0000

    Datical DB Deployment Packager Deployment Pipeline: ECloudPipeline Deploy / Rollback Scripts Processed (Associated Labels): ddl\change6.sql / none (change6.sql,$all)

---
+++ Resources/sql_scripts/20170615161552_change6.sql
@@ -0,0 +1,18 @@
+USE [ecloudintegration_ref];
+
+:OUT "C:\datical\ECloudIntegrationDemo\Resources\sql_scripts\20170615161552_change6.log"
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Region5](
+ [RegionID] [int] NOT NULL,
+ [RegionDescription] [nchar](50) NOT NULL,
+ CONSTRAINT [PK_Region5] PRIMARY KEY NONCLUSTERED
+(
+ [RegionID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+:OUT stdout
